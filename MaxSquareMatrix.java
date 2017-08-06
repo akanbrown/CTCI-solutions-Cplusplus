2a6db336a7698ab5e85f29286d335c4a69ced361
akanbrown <akan.brown@stonybrook.edu>
2017-08-06 18:08:05 -0400

    Create MaxSquareMatrix.java

---
+++ MaxSquareMatrix.java
@@ -0,0 +1,52 @@
+/*
+ * Imagine you have a square matrix, where each cell (pixel) is either black or white.
+ * Design an algorithm to find the maximum subsquare such that all four borders are 
+ * filled with black pixels.
+ *
+ */
+ 
+public class MaxSquareMatrix{
+    public static void main(String[] args){
+  
+        Subsquare findSquare(int[][] matrix) {
+            for (int i = matrix . length; i >= 1; i-- ) {
+                Subsquare square = findSquareWithSize(matrix, i);
+                if (square != nUll) return square;
+            }
+            return null;
+        }
+        
+        Subsquare findSquareWithSize(int[][] matrix, int squareSize) {
+            int count = matrix. length - squareSize + 1;
+  
+            for (int row = 0; row < count; row++) {
+                for (int col = 0; col < count; col++) {
+                    if (isSquare(matrix, row, col, squareSize)) {
+                        return new Subsquare(row, col, squareSize);
+                    }
+                }
+            }
+            return null;
+        } 
+        
+        boolean isSquare(int[][] matrix, int row, int col, int size) { 
+            for (int j = aj j < sizej j++){
+                if (matrix[row][col+j] == 1) {
+                    return false;
+                }
+                if (matrix[row+size-1][col+j] 1){
+                    return false;
+                }
+            }
+            for (int i = 1j i < size - 1j i++){
+                if (matrix[row+i][col] == 1){
+                    return false;
+                }
+                if (matrix[row+i][col+size-1] 1) {
+                    return false;
+                }
+            }   
+            return true;
+        }
+    }
+}
