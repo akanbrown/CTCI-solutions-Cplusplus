ad5ca39a860334826b4f824dd351f34309abee99
Akan Brown <akan.brown@stonybrook.edu>
2017-12-23 15:35:13 -0500

    Update MaxSquareMatrix.java

--- MaxSquareMatrix.java
+++ MaxSquareMatrix.java
@@ -1,5 +1,5 @@
 /*
- * Imagine you have a square matrix, where each cell (pixel) is either black or white.
+ * You are given a square matrix in which each cell (pixel) is either black or white.
  * Design an algorithm to find the maximum subsquare such that all four borders are 
  * filled with black pixels.
  *
