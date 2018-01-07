2359e4dcdbdd74bd9c0583e91a17507a5ae06162
Akan Brown <akan.brown@stonybrook.edu>
2018-01-07 09:09:14 -0500

    Update 2_5 13.cpp

--- 2_5 13.cpp
+++ 2_5 13.cpp
@@ -1,1 +1,14 @@
 
+/*
+
+Given a xircular linked list, implemwnt an algorithm which 
+returna node at the beginning of the loop
+DEFINITION:
+Circular linked list: a corrupt linked list in
+which a node's next pointer points to an earlier node,
+so as to make a loop in the linked list.
+EXAMPLE
+Input: A -> B -> C -> D -> E -> C [The same C as earlier]
+Output: C
+
+*/
