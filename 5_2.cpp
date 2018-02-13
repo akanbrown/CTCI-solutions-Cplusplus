60006670229f94de7b07deb934b21409e9a4385c
Akan Brown <akan.brown@stonybrook.edu>
2018-02-13 00:18:44 -0500

    Update 5_2.cpp

--- 5_2.cpp
+++ 5_2.cpp
@@ -1,4 +1,7 @@
-
+/*
+Given a number passed in as a string, 
+print the binary representation. If the number can not be represented accurately in binary, print "ERROR".
+*/
 #include <string>
 #include <bitset>
 #include <iostream>
@@ -9,7 +12,7 @@ int main(){
   {
       cout << bitset<8>(myString.c_str()[i]) << endl;
   }
-  
+  print("%s", "ERROR");
   system("pause");
   return 0;
 }
