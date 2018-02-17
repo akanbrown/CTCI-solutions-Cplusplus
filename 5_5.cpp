aa8b413de79e53ae093d468eff56dc882d86ac31
Akan Brown <akan.brown@stonybrook.edu>
2018-02-16 23:30:41 -0500

    Update 5_5.cpp

--- 5_5.cpp
+++ 5_5.cpp
@@ -3,3 +3,15 @@
 #include <iostream>
 using namespace std;
  
+
+// Function that count set bits
+int countSetBits(int n)
+{
+    int count = 0;
+    while (n)
+    {
+        count += n & 1;
+        n >>= 1;
+    }
+    return count;
+}
