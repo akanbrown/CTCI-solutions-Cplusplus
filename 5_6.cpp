6bb5c6c1441970812f833bc90e57bf6fbff29b3a
Akan Brown <akan.brown@stonybrook.edu>
2018-02-17 09:36:48 -0500

    Create 5_6.cpp

---
+++ 5_6.cpp
@@ -0,0 +1,15 @@
+/**
+Write a program to swap odd and even bits in an
+integer with as few instructions as possible (e.g., bit 
+0 and it 1 are swapped, bit 2 and bit 3 are swapped, etc);
+**/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+  return 0;
+  system("pause");
+  }
