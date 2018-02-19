dd9f61e858bee4f891292cca838cf5d348f5175a
Akan Brown <akan.brown@stonybrook.edu>
2018-02-18 22:39:51 -0500

    Update 5_7.cpp

--- 5_7.cpp
+++ 5_7.cpp
@@ -5,3 +5,5 @@ cannot access an entire integer in A with a single operation.
 The elements of A are represented in binary, and the only operation
 we can use to access them is "fetch the jth bit oof A[i]", which takes 
 constant time. Write code to find the missing integer. Can you do it in O(n) time?
+**/
+
