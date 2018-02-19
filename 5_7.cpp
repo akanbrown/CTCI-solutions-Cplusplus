8bca8e23d43024ac5cccbda566f032023e2a3b6a
Akan Brown <akan.brown@stonybrook.edu>
2018-02-18 22:39:25 -0500

    Update 5_7.cpp

--- 5_7.cpp
+++ 5_7.cpp
@@ -1,3 +1,5 @@
 /**
 An array A[1 ... n] contains all the integers from 0 to n 
 except for one number which is missing. In this problem, we 
+cannot access an entire integer in A with a single operation. 
+The elements of A are represented in binary, and the only operation
