f2ac72b83dcafe34b8d851f9cf2a16ab4723b23f
Akan Brown <akan.brown@stonybrook.edu>
2018-02-19 08:07:49 -0500

    Update 6_1.cpp

--- 6_1.cpp
+++ 6_1.cpp
@@ -1,9 +1,11 @@
 
+
 /**
 Add arithmetic operators (plus, minus, times, divide) to make
 the following expression true: 3 1 3 6  = 8. You can use any 
 parentheses you'd like.
 **/
+#include <iostream>
 
 int main(){
   
