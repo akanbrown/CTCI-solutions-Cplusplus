f6791e3c3da60350f1e3a91817372e80f1e5f0c3
Akan Brown <akan.brown@stonybrook.edu>
2018-02-23 11:48:10 -0500

    Update 6_4.cpp

--- 6_4.cpp
+++ 6_4.cpp
@@ -8,3 +8,4 @@ himself. To remove the hat, those (and only those who
 have a hat) must dunk themselves underwater at exactly 
 midnight. If there are n people and c hats, how long does 
 it take the men to remove the hats? The men cannot tel each 
+other (in any way) that they have a hat. Prove that our 
