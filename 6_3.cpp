c8935f72fa94e8596b2d245fef6d0ed3c3600528
Akan Brown <akan.brown@stonybrook.edu>
2018-02-22 19:29:32 -0500

    Update 6_3.cpp

--- 6_3.cpp
+++ 6_3.cpp
@@ -5,3 +5,17 @@ exactly four quarts of water? NOTE: The jugs are oddly shaped, such
 that filling up exactly 'half' of the jug would be impossible.
 **/
 
+#include <iostream>
+
+int main(){
+  printf("We can pour water back and forth between the two jugs as follows:" +
+         "5 quart contents\t\t3 Quart Contents\t\tNote" +
+         "5\t\t0\t\tFilled 5 quart jug" +
+         "2\t\t3\t\tFilled 3Q with 5Q's contents" +
+         "0\t\t2\t\tDumped 3Q" +
+         "5\t\t2\t\tFilled 5Q" +
+         "4\t\t3\t\tFilled remainder of 3Q with 5Q" +
+         "4\t\t\t\t Done! We have four quarts");
+  system("pause");
+  return 0;
+}
