a9e735f7c51b721e529dfe2b6cbdb65fc9f02e0d
Akan Brown <akan.brown@stonybrook.edu>
2018-02-23 11:47:53 -0500

    Update 6_4.cpp

--- 6_4.cpp
+++ 6_4.cpp
@@ -4,3 +4,5 @@ down and gathers everyone together and places a
 magical hat on some people's heads (i.e., at least 
 one person has a hat). The hat is magical: it can be 
 seen by other people, but not by the wearer of the hat 
+himself. To remove the hat, those (and only those who 
+have a hat) must dunk themselves underwater at exactly 
