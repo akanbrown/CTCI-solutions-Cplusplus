84c9392e4b0ad6e1eba3edcf8b02f595824a0cc7
Akan Brown <akan.brown@stonybrook.edu>
2018-02-23 11:47:44 -0500

    Update 6_4.cpp

--- 6_4.cpp
+++ 6_4.cpp
@@ -2,3 +2,5 @@
 A bunch of men are on an island. A genie comes 
 down and gathers everyone together and places a 
 magical hat on some people's heads (i.e., at least 
+one person has a hat). The hat is magical: it can be 
+seen by other people, but not by the wearer of the hat 
