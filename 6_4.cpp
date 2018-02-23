26a486820598ab8c9006d1c9ee7084895ba7f907
Akan Brown <akan.brown@stonybrook.edu>
2018-02-23 11:48:01 -0500

    Update 6_4.cpp

--- 6_4.cpp
+++ 6_4.cpp
@@ -6,3 +6,5 @@ one person has a hat). The hat is magical: it can be
 seen by other people, but not by the wearer of the hat 
 himself. To remove the hat, those (and only those who 
 have a hat) must dunk themselves underwater at exactly 
+midnight. If there are n people and c hats, how long does 
+it take the men to remove the hats? The men cannot tel each 
