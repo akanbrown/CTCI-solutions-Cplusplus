54d020e04e5954d72185013f0111de70b4dd4bcd
Akan Brown <akan.brown@stonybrook.edu>
2018-02-24 12:19:47 -0500

    Create 6_6.cpp

---
+++ 6_6.cpp
@@ -0,0 +1,9 @@
+/**
+There are one hundred closed lockers in a hallway. 
+A man begins by opening all one hundred lockres. Next, 
+he closes every second locker. Then he oes to every third 
+locker and closes it if it is open or opens it if it is closed 
+(e.g., he toggles every third locker). After his one hundredth 
+pass in the hallway, in which he toggles only locker number one 
+hundred, how many lockers are open?
+**/
