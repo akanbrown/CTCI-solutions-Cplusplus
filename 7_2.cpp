d2904abf0b97cd45c11cb6e9ae4ee597f3a12a9d
Akan Brown <akan.brown@stonybrook.edu>
2018-02-26 11:45:39 -0500

    Create 7_2.cpp

---
+++ 7_2.cpp
@@ -0,0 +1,14 @@
+/** Imagine you have a call center with three levels of employees: fresher, technical lead (TL),
+ *  product manager (PM). There can be multiple employees, but only one TL or PM.
+ * 
+ *
+ */
+ 
+ #include <iostream>
+ 
+ using namespace std;
+ 
+ int main(){
+  printf("Hello world\n");
+  system("pause");
+  return 0;
