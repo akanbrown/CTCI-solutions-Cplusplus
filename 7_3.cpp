3b5d8e6b83b6844beafdaa0f20ad48b8dfc91461
Akan Brown <akan.brown@stonybrook.edu>
2018-02-28 12:09:38 -0500

    Update 7_3.cpp

--- 7_3.cpp
+++ 7_3.cpp
@@ -2,8 +2,17 @@
 Design a musical juke box using object oriented principles.
 **/
 
-int main(){
-  
-  system("pause");
-  return 0;
+private: System::Void listBox1_SelectedIndexChanged(
+         System::Object^  sender, System::EventArgs^  e)
+{
+
+    label8->Text="";
+    label1->Text="Jokefile= "+fileList[listBox1->SelectedIndex] ;
+    ProcessFile( fileList[listBox1->SelectedIndex] );
+    jokeFileCount=listBox1->SelectedIndex;
+    listBox2->SetSelected(0,true);
+    updateInfoDisplay();
+    this->Text ="Joke Jukebox by TopCoder  [" + 
+                   fileList[listBox1->SelectedIndex]+"]";
 }
+
