0cb9e6edc2187cd1d9fa3515b31eca7c18e2e5ab
Akan Brown <akan.brown@stonybrook.edu>
2018-03-01 22:43:37 -0500

    Update 7_4.cpp

--- 7_4.cpp
+++ 7_4.cpp
@@ -3,9 +3,129 @@ Desing a chess game using object
 oriented principles
 **/
 
-int main(){
-  
-  
-  system("pause");
-  return 0;
-}
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Player
+{
+	int piece;
+public:
+	bool win();
+	bool lose();
+};
+
+class Piece 
+{
+	int value;
+public:
+	void move();
+	void capture();
+};
+
+class King : public Piece
+{
+public:
+	void castle_king();
+	void castle_queen();
+	void check();
+};
+
+class Queen : public Piece
+{
+
+};
+
+class Rook : public Piece
+{
+public:
+	void castle_king();
+	void castle_queen();
+};
+
+class Knight : public Piece
+{
+
+};
+
+class Bishop : public Piece
+{
+
+};
+
+class Pawn : public Piece
+{
+public:
+	void en_passant();
+};
+
+int main()
+{
+	char board[8][8];
+	string move_from,move_to;
+
+	cout << " Black" << endl;
+
+	board[0][0]='R';
+	board[0][1]='N';
+	board[0][2]='B';
+	board[0][3]='Q';
+	board[0][4]='K';
+	board[0][5]='B';
+	board[0][6]='N';
+	board[0][7]='R';
+
+	
+	for(int i=0;i<8;i++)
+	{
+	cout << board[0];
+	}
+	cout << endl;
+
+	for (int i=0;i<8;i++)
+	{
+	board[1]='P';
+	}
+	for(int i=0;i<8;i++)
+	{
+	cout << board[1];
+	}
+	cout << endl;
+	for(int i=0;i<8;i++)
+	{
+	board[2]='*';
+	board[3]='*';
+	board[4]='*';
+	board[5]='*';
+	}
+	for(int j=2;j<6;j++)
+	{
+	for(int i=0;i<8;i++)
+	{
+	cout << board[j];
+	}
+	cout << endl;
+	}
+	for (int i=0;i<8;i++)
+	{
+	board[6]='P';
+	}
+	for(int i=0;i<8;i++)
+	{
+	cout << board[6];
+	}
+	cout << endl;
+
+	board[7][0]='R';
+	board[7][1]='N';
+	board[7][2]='B';
+	board[7][3]='Q';
+	board[7][4]='K';
+	board[7][5]='B';
+	board[7][6]='N';
+	board[7][7]='R';
+
+	for(int i=0;i<8;i++)
+	{
+	cout << board[7];
