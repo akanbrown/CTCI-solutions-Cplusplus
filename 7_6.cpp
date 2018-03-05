7cd46384188dfe6cb1f50b09607cdd9374ac3f00
Akan Brown <akan.brown@stonybrook.edu>
2018-03-05 01:09:45 -0500

    Update 7_6.cpp

--- 7_6.cpp
+++ 7_6.cpp
@@ -1,3 +1,29 @@
 /**
 Implement a jigsaw puzzle. Design the data structures and explain an algorithm to solve the puzzle.
 **/
+public class JigsawPuzzle {
+	public Piece[] piecesOfPuzzle;
+
+	public JigsawPuzzle(Piece[] pieces) {
+		this.piecesOfPuzzle = pieces;
+	}
+
+	public Piece[] piecesOfPuzzle() {
+		return this.piecesOfPuzzle;
+	}
+
+	public boolean solve() {
+		Piece aPiece = null;
+		Piece anotherPiece = null;
+
+		Stack<Piece> stackOfPieces = stackOfPieces();
+
+		while (stackOfPieces.size() > 1) {
+			aPiece = stackOfPieces.pop();
+
+			while (!stackOfPieces.isEmpty()) {
+				anotherPiece = stackOfPieces.pop();
+
+				if (aPiece.fitsWith(anotherPiece)) {
+					Piece bigPiece = aPiece.joinWith(anotherPiece);
+
