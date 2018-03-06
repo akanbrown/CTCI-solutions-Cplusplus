ab023853286c871cf06988bd4ea9755fb946314b
Akan Brown <akan.brown@stonybrook.edu>
2018-03-05 19:59:56 -0500

    Create 7_7.cpp

---
+++ 7_7.cpp
@@ -0,0 +1,7 @@
+/**
+
+Explain how you would designa chat server. In particular,
+provide details about he various backend components, classes, 
+and methods. What would be the hardest problem to solve?
+
+**/
