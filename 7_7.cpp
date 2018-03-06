01fe58c538d68d04a273f5f54f5df093611afb32
Akan Brown <akan.brown@stonybrook.edu>
2018-03-05 20:02:43 -0500

    Update 7_7.cpp

--- 7_7.cpp
+++ 7_7.cpp
@@ -16,3 +16,13 @@ number o fusers. People hav ea contact list, they see who is online vs offline, 
 can send text-based messages to them. We will not worry about supporting group chat,
 voice chat, etc. We will also assume that the contact lists are mutual: I can only 
 talk to you if you can talk to me. Let's keep it simple.
+
+What specific actions does it need to suport?
+* User A signs online
+* User A asks for their contact list, with each person's current status.
+* Friends of User A now see User A as online
+* User A adds User B to contact list
+* User A sends text-based message to User B
+* User A changes status message and/or status type
+* User A removes User B
+* User A signs offline
