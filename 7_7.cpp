72a928816628e256af1947c70bd3bc8501d76999
Akan Brown <akan.brown@stonybrook.edu>
2018-03-05 20:01:44 -0500

    Update 7_7.cpp

--- 7_7.cpp
+++ 7_7.cpp
@@ -5,3 +5,14 @@ provide details about he various backend components, classes,
 and methods. What would be the hardest problem to solve?
 
 **/
+
+
+
+/**
+What is our chat server?
+This is something you should discuss with your interviewer, but let's make a couple of 
+assumptions: imagine we're designing a basic chat server tha tneeds to support a small
+number o fusers. People hav ea contact list, they see who is online vs offline, and they
+can send text-based messages to them. We will not worry about supporting group chat,
+voice chat, etc. We will also assume that the contact lists are mutual: I can only 
+talk to you if you can talk to me. Let's keep it simple.
