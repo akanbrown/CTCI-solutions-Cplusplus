43d77eece0109b10aed3abbad6b15e431465f9d5
Akan Brown <akan.brown@stonybrook.edu>
2018-03-07 00:32:11 -0500

    Update 7_9.cpp

--- 7_9.cpp
+++ 7_9.cpp
@@ -1,4 +1,13 @@
 /**
-Explain the data structures and algorithms tha tyou would use to design an in-memory file systme.Illustrate
+Explain the data structures and algorithms that you would use to 
+design an in-memory file systme. Illustrate
 with an example in code where possible.
 **/
+
+/**
+For data block allocation, we can use bitmask vector and linear search (see "practical
+File System Design") or B+ trees.
+**/
+struct DataBLock { char data[DATA_BLOCK_SIZE]; };
+DataBlock dataBLocks[NUM_DATA_BLOCKS];
+struct INode {std::vector<int> datablocks; };
