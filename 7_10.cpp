724c5d3ed1da05706c334a9921885376db093d32
Akan Brown <akan.brown@stonybrook.edu>
2018-03-09 23:44:39 -0500

    Create 7_10.cpp

---
+++ 7_10.cpp
@@ -0,0 +1,11 @@
+/**
+Describe the data structures and algorithms 
+that you would use to implement a garbage collector in C++.
+**/
+
+/**
+In C++, garbage collection with reference counting 
+is almost always implemented with smart pointers,
+which perform reference counting. The main reason 
+for using smart pointers over raw ordinary pointers
+is the conceptual simplicity of implementation and usage.
