fb7e4e51f7100d72d2171b53ea5b68eb2413c310
Akan Brown <akan.brown@stonybrook.edu>
2018-03-10 23:50:15 -0500

    Update 8_1.cpp

--- 8_1.cpp
+++ 8_1.cpp
@@ -6,4 +6,33 @@ Write a method to generate the nth fibonacci number
 
 
 public class Solution{
-b
+  public static int fib(int n){
+    if (n < 0) return -1; //error condition
+    
+    if (n==0 || n==1) return 1;
+
+    return (fib(n-1) + fib(n-2));
+  }
+
+  public static void main(String[] args){
+    for (int i=0;i<10;i++){
+      System.out.print(fib(i) + " ");
+    }
+    System.out.println();
+  }
+}
+
+
+/**
+Iterative solution:
+int fib(int n){
+  if (n < 0) return -1;
+  if (n == 0) return 0;
+  int a = 1, b=1;
+  for (int i=3; i<=n;i++){
+    int c =a + b;
+    a = b;
+    b = c;
+  }
+  return b;
+}
