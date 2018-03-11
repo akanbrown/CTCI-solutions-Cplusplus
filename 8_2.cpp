baed6667e8cf0872f69dac301026c838597d32c4
Akan Brown <akan.brown@stonybrook.edu>
2018-03-11 09:28:36 -0400

    Update 8_2.cpp

--- 8_2.cpp
+++ 8_2.cpp
@@ -11,3 +11,17 @@ step on them. Design an algorithm to get all possible paths for the robot.
 /**
 Part 1: (For clarity, we will solve this part assuming an X by Y grid)
 
+Each path has (X-1)+(Y-1) steps. Imagine the following paths:
+X X Y Y X (move right -> right -> down -> down -> right)
+X Y X Y X (move right -> down -> right -> down -> right)
+...
+Each path can be fully represented by the moves at which we move right. 
+That is, if I were to ask you which path you took, you could simply say
+"I moved right on step 3 and 4."
+
+Since you must always move right X-1 times, and you have X-1 + Y-1 total 
+steps, you have to pick X-1 times to move right out of X-1+Y-1 choices.
+Thus, there are C(X-1, X-1+Y-1) paths (e.g., X-1+Y-1 choose X-1):
+(X-1 + Y-1)! / ((X-1)! * (Y-1)!)
+
+Part 2: Code
