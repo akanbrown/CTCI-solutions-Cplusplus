7a3e5ae3d2064a552496cc4e673ca43a17915dac
Akan Brown <akan.brown@stonybrook.edu>
2018-03-10 23:50:35 -0500

    Update 8_1.cpp

--- 8_1.cpp
+++ 8_1.cpp
@@ -11,3 +11,18 @@ public class Solution{
     
     if (n==0 || n==1) return 1;
 
+    return (fib(n-1) + fib(n-2));
+  }
+
+  public static void main(String[] args){
+    for (int i=0;i<10;i++){
+      System.out.print(fib(i) + " ");
+    }
+    System.out.println();
+  }
+}
+
+
+/**
+Iterative solution:
+int fib(int n){
