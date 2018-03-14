4edd4240ecab6ba800db0a07b76c3384af3d9238
Akan Brown <akan.brown@stonybrook.edu>
2018-03-14 13:47:21 -0400

    Update 8_4.cpp

--- 8_4.cpp
+++ 8_4.cpp
@@ -5,3 +5,6 @@ all permutations of a string
 
 /**
 Let's assume a given string S represented by the letters A1, A2, A3, ..., An
+To permute set S, w ecan select the first character, A1, permute the remainder
+of the strin got get a new list. Then, with ha tnew list, we can "push" A1 into each possible position.
+**/
