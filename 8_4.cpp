c8aea8eb6959a1a1306b355cf2f11a01ecbbeaf5
Akan Brown <akan.brown@stonybrook.edu>
2018-03-14 13:48:27 -0400

    Update 8_4.cpp

--- 8_4.cpp
+++ 8_4.cpp
@@ -8,3 +8,9 @@ Let's assume a given string S represented by the letters A1, A2, A3, ..., An
 To permute set S, w ecan select the first character, A1, permute the remainder
 of the strin got get a new list. Then, with ha tnew list, we can "push" A1 into each possible position.
 **/
+
+/**
+For example, if our string is "abc", we would do the following: 
+1. Let first = "a" and let remainder = "bc"
+2. Let list = permute(bc) = ("bc", "cd")
+3. Push "a" into each location of "bc" (--> "abc", "bac", "bca") and "cb" (--> "acb", "cab", "cba")
