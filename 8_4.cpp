4ac1c7e3adbb8908f2f4022bfc77a9bec3ed77e2
Akan Brown <akan.brown@stonybrook.edu>
2018-03-14 13:54:46 -0400

    Update 8_4.cpp

--- 8_4.cpp
+++ 8_4.cpp
@@ -18,3 +18,33 @@ For example, if our string is "abc", we would do the following:
 
 Now the code to do this:
 **/
+
+public static ArrayList<string> getPerms(String s){
+  ArrayList<String> permutations = new ArrayList<string>();
+  if (s == null){ // error case
+    return null;
+  } else if (s.length() == 0){
+      permutations.add("");
+      return permuations;
+  }
+
+  char fist = s.charAt(0); //get the first character
+  String remainder = s.substring(1); //remove the first character
+  ArrayList<String> words = getPerms(remainder);
+  for (String word : words){
+    for (int j=0;j<=word.length();j++){
+      permutations.add(insertCharAt(word, first, j));
+    }
+  }
+  return permutations;
+}
+
+public static String insertCharAt(String word, char c, int i){
+  String start = word.substring(0, i);
+  String end = word.substring(i);
+  return start + c + end;
+}
+
+/**
+This solution takes O(n!) time, since there are n! permutations. 
+**/
