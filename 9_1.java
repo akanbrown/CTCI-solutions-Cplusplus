06169e399e620bc002304c697a1781481235ad11
Akan Brown <akan.brown@stonybrook.edu>
2018-03-19 20:23:35 -0400

    Update 9_1.java

--- 9_1.java
+++ 9_1.java
@@ -10,3 +10,10 @@ The code is part of the standard merge-sort code. We merge
 A and B from the back, by comparing each element.
 
 **/
+
+public static void merge(int[] a, int[] b, int n, int m){
+  int k = m + n - 1; //Index of last location of array b
+  int i = n - 1; //Index of last element in array b
+  int j = m - 1; //Index of last element in array a
+  
+  //Start comparing from the last element and merge a and b
