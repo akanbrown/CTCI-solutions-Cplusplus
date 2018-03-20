937576e62d50f4beee6cfc4cd4c8ca2199f7e390
Akan Brown <akan.brown@stonybrook.edu>
2018-03-19 20:22:00 -0400

    Update 9_1.java

--- 9_1.java
+++ 9_1.java
@@ -5,3 +5,8 @@ to hold B. Write a method to merge B into A in sorted order.
 
 **/
 
+/**
+The code is part of the standard merge-sort code. We merge
+A and B from the back, by comparing each element.
+
+**/
