85dad5d17e0dd276435e171faeb193d207400018
Akan Brown <akan.brown@stonybrook.edu>
2018-03-25 21:19:56 -0400

    Update 9_3.java

--- 9_3.java
+++ 9_3.java
@@ -1,2 +1,22 @@
 /**
-Write a me
+Given a sorted array of n integers that has been rotated an unknown number of times, give an O(log n) algorithm tha tfinds an element in the array. You may assume that the array was 
+originally sored in increasing order.
+
+EXAMPLE:
+Input: find 5 in array (15 15 19 20 25 1 3 4 5 7 10 14)
+Output: 8 (the index of 5 in the array)
+
+**/
+/**
+SOLUTION
+
+We can do this with a modification of binary search.
+**/
+
+public static in search (int a[], int l, int u, int x){
+  while (1 <= u){
+    int m = (1 + u)  / 2;
+    if (x == a[m]){
+      return ;
+    } else if (a[l] <= a[m]) { 
+      
